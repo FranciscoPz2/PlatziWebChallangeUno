18fe44f38a04b4dcea11bc29492f360f1601340e
Francisco Perez <efperez90@gmail.com>
2020-08-21 02:07:22 -0500

    agregando algo de js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 function displayForm() {  
     alert("ya se ha registrado anteriormente el día: " + Date());    
 }
+function fbForm() {  
+    alert("Bienvenido a facebook ");    
+}
+function twForm() {  
+    alert("Twitea todo " );    
+}
+function igForm() {  
+    alert("Siguenos en Instagram ");    
+}
